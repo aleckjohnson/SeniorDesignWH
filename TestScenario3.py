1acffc0d1ee1814cd1de9e9d8f6472a72281a6b6
Star LaGrasse <39280279+StarLaGrasse@users.noreply.github.com>
2019-04-16 00:04:57 -0400

    I am very smart and infinitely stupid
    Alright, a lot of new changes to just this one program so buckle up:
    OffLimitsArea is now gone
    -OffLimitsAreas no longer drawn while rendering
    -WifiNode class no longer has references to OffLimitsArea
    
    Node checking and calculation incredibly streamlined
    -Program is now back to reading each individual pixel
    -There is now a 2D boolean array that tells you what coordinates are populated
    -Nodes only spawn on roads on unpopulated coordinates
    -Rendering 4000 nodes takes about 5 minutes
    -No more weird gaps between nodes because algorithm no longer skips spaces
    
    Ctypes window replaced with tkinter window

--- TestScenario3.py
+++ TestScenario3.py
@@ -1,3 +1,6 @@
+from PIL import Image
+from tkinter import messagebox
+
 # OSMNX imports
 import sys		# For reading arguments from the command line.
 import networkx as nx
@@ -8,9 +11,8 @@ import matplotlib.colors as colors
 
 # PyGame imports
 import pygame
-import random
+
 import math
-import ctypes  # An included library with Python install.
 
 # Zoom function. Still needs to be implemented.
 # Find a way to pull from mouse (pynput library?) or zoom in and out using pygame. 
@@ -32,52 +34,11 @@ def makeGraph(city, state, country):
 
 makeGraph(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))
 
-
-#import OffLimitArea
-class OffLimitArea:
-    #A rectangle that nothing is allowed inside of
-    _origin = [0, 0] #top left corner
-    _end = [0, 0] #bottom right corner
-
-    def __init__(self, orig, en):
-        self._origin = orig
-        self._end = en
-        #print ("Area off limits within", self._origin, "and", self._end)
-
-    def setArea(self, orig, en):
-        self._origin = orig
-        self._end = en
-        print("New area off limits within", self._origin, "and", self._end)
-
-    def contains(self, x, y):
-        #is the specified point within the defined area? if yes return true, otherwise return false
-        print("Running check in area between ", self._origin, " and ", self._end)
-        if (x >= self._origin[0]) and (x <= self._end[0]):
-            if (y >= self._origin[1]) and (y <= self._end[1]):
-                return True
-            else:
-                return False
-        else:
-            return False
-
-    def getOrigX(self):
-        return self._origin[0]
-
-    def getOrigY(self):
-        return self._origin[1]
-
-    def getEndX(self):
-        return self._end[0]
-
-    def getEndY(self):
-        return self._end[1]
-
 #import WifiNode
 
 class WifiNode:
     #a circle representing the wifi node
     _origin = [0, 0] #the origin point
-    _notAllowed = OffLimitArea([0, 0], [0, 0]) #an area within the node where no other nodes should spawn
     _frequecy = 0 #input frequency in MHz
     #all rads in km
     _rad30 = 0 #Best Coverage
@@ -93,8 +54,6 @@ class WifiNode:
         self._origin = orig
         print("Created a wifi node at ", self._origin, " with a frequency of ", self._frequency,"MHz")
         self._piPerKM = ppk
-        x = orig[0]
-        y = orig[1]
         self._rad30 = math.pow(float(10),
                                (float(30) - float(32.44) - (float(20)*math.log10(self._frequency)))/float(20))
         self._rad67 = math.pow(float(10),
@@ -105,22 +64,12 @@ class WifiNode:
                                (float(80) - float(32.44) - (float(20) * math.log10(self._frequency))) / float(20))
         self._rad90 = math.pow(float(10),
                                (float(90) - float(32.44) - (float(20) * math.log10(self._frequency))) / float(20))
-        marg = int(self._rad80 * self._piPerKM)
-        self._notAllowed.setArea([x - marg, y - marg], [x + marg, y + marg])
-
-
-    def contains(self, x, y):
-        print("Running check from node at ", self._origin)
-        contains = self._notAllowed.contains(x, y)
-        return contains
 
     def setArea(self, orig, freq, ppk):
         self._frequency = freq
         self._origin = orig
         print("Changed a wifi node at ", self._origin, " with a frequency of ", self._frequency, "MHz")
         self._piPerKM = ppk
-        x = orig[0]
-        y = orig[1]
         self._rad30 = math.pow(float(10),
                                (float(30) - float(32.44) - (float(20) * math.log10(self._frequency))) / float(20))
         self._rad67 = math.pow(float(10),
@@ -131,8 +80,6 @@ class WifiNode:
                                (float(80) - float(32.44) - (float(20) * math.log10(self._frequency))) / float(20))
         self._rad90 = math.pow(float(10),
                                (float(90) - float(32.44) - (float(20) * math.log10(self._frequency))) / float(20))
-        marg = int(self._rad80 * self._piPerKM)
-        self._notAllowed.setArea([x - marg, y - marg], [x + marg, y + marg])
 
     def getFrequency(self):
         return self._frequency
@@ -161,7 +108,15 @@ class WifiNode:
     def getOrigin(self):
         return self._origin
 
-size = [750, 750]
+
+im = Image.open("images/temp.png")
+pix = im.load()
+size = [im.size[0], im.size[1]]
+
+nodeList = []
+noNoSquareList = []
+streets = []
+
 WHITE = [255, 255, 255, 255]
 BLACK = [0, 0, 0, 255]
 BLUE = [0, 0, 255, 255]
@@ -175,79 +130,37 @@ RED = [255, 0, 0, 50]
 pixPerKM = 100 #Pixels per kilometer conversion
 frequency = float(2400)
 
+populated = []
+for x in range(size[0]):
+    newArr = []
+    for y in range(size[1]):
+        newArr.append(False)
+    populated.append(newArr)
 
-nodeList = []
-noNoSquareList = []
-
-for i in range(random.randint(int(size[0]/20), int(size[0]/10))):
-    x = random.randint(0, size[0])
-    y = random.randint(0, size[1])
-    xdif = random.randint(19, 100)
-    ydif = random.randint(19, 100)
-    noNoSquareList.append(OffLimitArea([x, y], [x + xdif, y + ydif]))
-
-x = 0
-y = 0
-cont = True
-while cont:
-    cont = False
-    for o in noNoSquareList:
-        if o.contains(x, y):
-            cont = True
-            break
-    if cont:
-        if x < size[0]:
-            x += 1
+#read pixels
+for y in range(size[1]):
+    for x in range(size[0]):
+        if pix[x, y][0] == 255 and pix[x, y][1] == 255 and pix[x, y][2] == 255:
+            continue
+        if populated[x][y]:
+            continue
         else:
-            x = 0
-            y += 1
-        if y > size[1]:
-            print("Not possible to spawn initial node")
-            break
-
-if not cont:
-    nodeList.append(WifiNode([x, y], frequency, pixPerKM))
-
-while y <= size[1]:
-    addRad = max(int(nodeList[len(nodeList) - 1].getRad80() * pixPerKM), 1)
-    if x < size[0]:
-        x += addRad
-    else:
-        while x > 0:
-            x -= addRad
-        if x < 0:
-            x += addRad
-        if x > size[0]:
-            x = 0
-        y += addRad
-    if y > size[1]:
-        print("Exceeded size limit")
-        break
-    else:
-        newLoc = [x, y]
-        print("Testing location ", newLoc)
-        good = True
-        if (x > size[0]) or (y > size[1]):
-            good = False
-        else:
-            for o in noNoSquareList:
-                if o.contains(x, y):
-                    print(newLoc, " in off limits area")
-                    good = False
-                    break
-
-        if good:
-            print(newLoc, " passed")
+            newLoc = [x, y]
             nodeList.append(WifiNode(newLoc, frequency, pixPerKM))
-
-        print("Current number of nodes: ", len(nodeList))
-totalNodes = len(nodeList)
+            lenny = len(nodeList)
+            print("Current number of nodes: ", lenny)
+            lenny -= 1
+            node = nodeList[lenny]
+            marg = int(node.getRad80() * pixPerKM)
+            for tempx in range(max(0, x - marg), min(size[0], x + marg + 1)):
+                for tempy in range(max(0, y - marg), min(size[1], y + marg + 1)):
+                    populated[tempx][tempy] = True
 
 print("Nodes calculated. Prepare to print to screen!")
+totalNodes = len(nodeList)
 
 screen = pygame.display.set_mode(size)
 back = pygame.image.load("images/temp.png")		# Image saved from previous run.
-back = pygame.transform.scale(back, (750, 750))
 surface = pygame.Surface(size, pygame.SRCALPHA, 32)
 
 # this loop is used to keep the window opened until user closes out of window
@@ -258,10 +171,7 @@ surfaces = []
 #this line is used to clear the window and set background color (later background will be a portion of a location on a map)
 screen.fill(WHITE)
 
-#draw the off limit area
-for o in noNoSquareList:
-    pygame.draw.rect(screen, BLACK, pygame.Rect(o.getOrigX(), o.getOrigY(), o.getEndX() - o.getOrigX(), o.getEndY() - o.getOrigY()))
-    screen.blit(back,(0,0))
+
 #draw all the nodes in the list
 for n in nodeList:
     surf = pygame.Surface(size, pygame.SRCALPHA, 32)
@@ -282,8 +192,7 @@ pygame.display.flip()
 ciscoCost = totalNodes * 3000
 ruckusCost = totalNodes * 3500
 print("The Estimated Associated cost for "+str(totalNodes)+" Nodes is: Cisco Aironet 1572EAC: $"+str(ciscoCost)+" Ruckus T811-CM: $"+str(ruckusCost))
-ctypes.windll.user32.MessageBoxW(0, "The Estimated Associated cost for "+str(totalNodes)+" Nodes is: Cisco Aironet 1572EAC: $"+str(ciscoCost)+" Ruckus T811-CM: $"+str(ruckusCost),"Estimated Node Cost", 1)
-
+messagebox.showinfo("Estimated Costs", "The Estimated Associated cost for "+str(totalNodes)+" Nodes is: Cisco Aironet 1572EAC: $"+str(ciscoCost)+" Ruckus T811-CM: $"+str(ruckusCost))
 while not done:
     clock.tick(10)
 
@@ -292,3 +201,4 @@ while not done:
             done = True
 
 pygame.quit()
+
